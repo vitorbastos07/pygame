27357cd0fb702b95c155413c11242380a4266cfb
Gabriel Mariano <gabrielmarianof@icloud.com>
2025-05-26 16:35:25 -0300

    configg

---
+++ src/config.py
@@ -0,0 +1,14 @@
+import pygame	
+sapo_width = 50
+sapo_height = 60
+
+carro_width = 100
+carro_height = 80
+barco_width = 150
+barco_height = 80
+
+WIDTH = 750
+HEIGHT = 1000
+sapo_y_initial = (HEIGHT - sapo_height + 20)
+sapo_x = (WIDTH-sapo_width) / 2
+sapo_y = sapo_y_initial
